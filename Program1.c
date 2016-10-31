6ad11720efc3caacc847702493199108c6f9e659
postmodern-cereal <noah.ransom@maine.edu>
2016-10-31 12:41:09 -0400

    Added programs for assignment
    Completed program 1 only.

---
+++ Program1.c
@@ -0,0 +1,18 @@
+/*write c function that prints:
+    name from arg vector
+    process ID using getPID
+    2-3 print statements of your chosing
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+main(int argc, char *argv[])
+{
+    Printf("This is program 1. It prints its name and ID.\n");
+    printf("Process name: %s\n", argv[0]);
+    printf("Process ID: %d\n", getpid());
+    printf("Program 1 has finsihed execution.\n");
+}
